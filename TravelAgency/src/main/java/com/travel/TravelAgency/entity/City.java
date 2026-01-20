e8a5ce8d4e13c10f153851e6ad6431ccbeb3f33f
kume <kumegjika@hotmail.com>
2026-01-20 22:19:16 +0100

    add Country and City entities

--- TravelAgency/src/main/java/com/travel/TravelAgency/entity/City.java
+++ TravelAgency/src/main/java/com/travel/TravelAgency/entity/City.java
@@ -1,4 +1,22 @@
 package com.travel.TravelAgency.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "cities")
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String	name;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "continent_id")
+    private Continent membership;
+
 }
--- TravelAgency/src/main/java/com/travel/TravelAgency/entity/Country.java
+++ TravelAgency/src/main/java/com/travel/TravelAgency/entity/Country.java
@@ -1,4 +1,23 @@
 package com.travel.TravelAgency.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "countries")
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name = "continent_id")
+    private Continent continent;
+
 }
