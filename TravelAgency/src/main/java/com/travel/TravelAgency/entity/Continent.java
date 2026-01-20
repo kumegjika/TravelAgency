8211fee9889f6748c40254b74fe4d5266e2e619e
kume <kumegjika@hotmail.com>
2026-01-20 11:09:27 +0100

    Initial project structure

--- TravelAgency/src/main/java/com/travel/TravelAgency/entity/Continent.java
+++ TravelAgency/src/main/java/com/travel/TravelAgency/entity/Continent.java
@@ -1,4 +1,20 @@
 package com.travel.TravelAgency.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "continents")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Continent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
