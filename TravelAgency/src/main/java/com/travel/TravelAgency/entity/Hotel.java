2c84df69e857d9ccd9966d0cb645b3f04be99b27
kume <kumegjika@hotmail.com>
2026-01-27 19:58:30 +0100

    add hotel entity

--- TravelAgency/src/main/java/com/travel/TravelAgency/entity/Hotel.java
+++ TravelAgency/src/main/java/com/travel/TravelAgency/entity/Hotel.java
@@ -1,4 +1,27 @@
 package com.travel.TravelAgency.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "hotels")
 public class Hotel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private String name;
+    private int standard;
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private City designatedCity;
+
 }
