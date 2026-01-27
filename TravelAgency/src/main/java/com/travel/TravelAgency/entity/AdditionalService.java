53c20f15b743fe14493ad33e7c8aee5b1841d1af
kume <kumegjika@hotmail.com>
2026-01-27 20:24:44 +0100

    add users AdditionalService

---
+++ TravelAgency/src/main/java/com/travel/TravelAgency/entity/AdditionalService.java
@@ -0,0 +1,27 @@
+package com.travel.TravelAgency.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "additional_services")
+public class AdditionalService {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(length = 1000)
+    private String description;
+
+    private double priceAdult;
+
+    private double priceChild;
+}
